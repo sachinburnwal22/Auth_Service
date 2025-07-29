12a3dca5ada4f5ec2d6f95cceeb1ebb5dc6f5f6c
SACHIN BURNWAL <sachinburnwal22@gmail.com>
2025-07-29 11:25:42 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const apiRoutes = require("./routes/index");
 // const bcrypt = require('bcrypt');
 // const UserRepository = require('./repository/user-repository');
 
+const UserService = require("./services/user-service");
+
 const app = express();
 
 const prepareAndStartServer = () => {
@@ -18,13 +20,15 @@ const prepareAndStartServer = () => {
 
   app.listen(PORT, async () => {
     console.log(`Server Started on Port: ${PORT}`);
-    // const repo = new UserRepository();
-    // const response = await repo.getById(1);
-    // console.log(response);
-    // const incomingpassword = '123456';
-    // const user = await User.findByPk(3);
-    // const response = bcrypt.compareSync(incomingpassword, user.password);
-    // console.log(response);
+    const service = new UserService();
+    // const newToken = service.createToken({ email: "sanket@admin.com", id: 1 });
+    // console.log("new token is", newToken);
+
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbmtldEBhZG1pbi5jb20iLCJpZCI6MSwiaWF0IjoxNzUzNzY2ODIyLCJleHAiOjE3NTM3NzA0MjJ9.C-sMtF-mF3_AHp7zy40sy1rAsYLdITe0vFse84WLG6I";
+
+    const response = service.verifyToken(token);
+    console.log(response);
   });
 };
 
